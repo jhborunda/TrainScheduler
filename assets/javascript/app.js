bb6fd7ae30201334f152b15530f168b38e05ba80
Jose Borunda <jose.h.borunda@gmail.com>
2019-07-31 20:21:18 -0600

    started html with bootstrap and added firebase to app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,18 @@
 //Need to create Firebase database
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAPomo8wpIbIusbR9epa3YjKzG0a4WS9QU",
+    authDomain: "trainscheduler-d763e.firebaseapp.com",
+    databaseURL: "https://trainscheduler-d763e.firebaseio.com",
+    projectId: "trainscheduler-d763e",
+    storageBucket: "",
+    messagingSenderId: "764713174359",
+    appId: "1:764713174359:web:fec8ff65c896b2c0"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-
-//Firebase needs to be initialized
+//need to create variables for inputs
 
 
 //Need to create funtions to append data that has been submitted
