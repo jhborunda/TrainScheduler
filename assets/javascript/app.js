c99a512006ac9ef2bac19bf746473f9b6b4a3625
Jose Borunda <jose.h.borunda@gmail.com>
2019-07-31 19:59:29 -0600

    added style and js files, pseudo coded js

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//Need to create Firebase database
+
+
+//Firebase needs to be initialized
+
+
+//Need to create funtions to append data that has been submitted
+
+
+//need to create an onclick for data inputed
